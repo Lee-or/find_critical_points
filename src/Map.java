a5a391f2372b667241665cb4fc49a1e123dd3d1a
Lee-or <Lee-or@bitbucket.org>
2017-10-16 01:23:54 +0300

    findCriticalPointsInMap - not final. firstPixelOfAnObstacle - not final.

--- src/Map.java
+++ src/Map.java
@@ -1,5 +1,3 @@
-import javafx.scene.image.Image;
-
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -9,12 +7,13 @@ import java.util.List;
  * Created by Lee-or on 02/10/2017.
  */
 public class Map {
-    private Image mapImg;
+    private BufferedImage mapBuffImg;
     private byte[][] binaryMap;
     private List<Point> criticalPoints;
 
-    public Map(Image givenMap){
-        this.mapImg = givenMap;
+
+    public Map(BufferedImage givenMap){
+        this.mapBuffImg = givenMap;
         this.criticalPoints = null;
     }
 
@@ -25,10 +24,10 @@ public class Map {
         int height  = image.getHeight();
         byte[][] pixels = new byte[width][];
 
-        for (int x = 0; x < width; x++) {
+        for (int x = 0; x < width; ++x) {
             pixels[x] = new byte[height];
 
-            for (int y = 0; y < height; y++) {
+            for (int y = 0; y < height; ++y) {
                 pixels[x][y] = (byte) (image.getRGB(x, y) == 0xFFFFFFFF ? 0 : 1);
             }
         }
@@ -36,11 +35,36 @@ public class Map {
         return pixels;
     }
 
+    // "All BufferedImage objects have an upper left corner coordinate of (0, 0)." (javadoc)
+    // x- row, y- col => x- width, y- height.
+    /**
+     * firstPixelOfAnObstacle
+     * @param map
+     * @param x x coordinate of the pixel the user wishes to check.
+     * @param y y coordinate of the pixel the user wishes to check.
+     * @param width width of the map.
+     * @param height the height of the map.
+     * @return true if the given pixel is a critical point. Otherwise, false.
+     */
+    /*TODO --PROBLEM-- this algorithm returns false if there are more than 1 pixel in row in the same column */
+    /*TODO this algorithm doesn't verify that there are no 2 critical points in the same vertical line*/
+    private boolean firstPixelOfAnObstacle(byte[][] map, int x, int y, int width, int height){
+        //if()
+    }
+
     /**
      * findCriticalPointsInMap.
      * @return array which holds the coordinates of all critical points in this Map.
      */
     private void findCriticalPointsInMap(){
+        int height = this.mapBuffImg.getHeight();
+        int width= this.mapBuffImg.getWidth();
+
+        for(int i=0; i<height; ++i){
+            for (int j=0; j<width; ++j){
+
+            }
+        }
 
     }
 
